a203af9766e4607e9e1a051bd37aec8bf3fb6e5f
OrtizJosh <joshwortiz@gmail.com>
2019-12-13 23:28:30 -0700

    mvp implementatiion

--- docs/script.js
+++ docs/script.js
@@ -1,46 +1,3 @@
-//Buttons to allow expand and collapse buttons.
-function parameterButton() {
-	$('#game-filters').click(function() {
-		$('#parameters').slideToggle();
-		$('.param-container').slideUp();
-		$('.button-text').empty();
-		if ($('.button-text').is('.menu-closed')) {
-			$('.button-text')
-				.addClass('menu-open')
-				.removeClass('menu-closed')
-				.append(`Close Filters`);
-		} else {
-			$('.button-text')
-				.addClass('menu-closed')
-				.removeClass('menu-open')
-				.append(`Open Filters`);
-		}
-	});
-}
-
-function platformButton() {
-	$('#platform-button').click(function() {
-		$('#platform-container').slideToggle();
-	});
-}
-
-function genreButton() {
-	$('#genre-button').click(function() {
-		$('#genre-container').slideToggle();
-	});
-}
-function contactButton() {
-	$('#contact-button').click(function() {
-		$('#contact-container').slideToggle();
-	});
-}
-
-function disclosureButton() {
-	$('#disclosure-button').click(function() {
-		$('#disclosure-container').slideToggle();
-	});
-}
-
 //This will generate the dates needed to fit critera.
 function generateDate() {
 	let date = new Date();
@@ -50,36 +7,26 @@ function generateDate() {
 		(date.getMonth() + 1) +
 		'-' +
 		date.getDate();
-	let predate = new Date(new Date().setDate(date.getDate() + 366));
-	let past =
-		predate.getFullYear() +
+	let postdate = new Date(new Date().setDate(date.getDate() + 1825));
+	let future =
+		postdate.getFullYear() +
 		'-' +
-		(predate.getMonth() + 1) +
+		(postdate.getMonth() + 1) +
 		'-' +
-		predate.getDate();
-	return `${past},${present}`;
+		postdate.getDate();
+	return `${present},${future}`;
 }
 
 //Parameters to filter results.
 const params = {
 	//Permanent Params
 	parent_platforms: '2,3,6,7',
-	page_size: '10',
+	page_size: '20',
 	//Adjustable Params
-	// genres: $(`input[type=checkbox][name=genre]:checked`).val()
-	platforms: $(`input[type=checkbox][name=platform]:checked`).val()
-	// dates: generateDate(),
-	// ordering: '-released'
+	dates: generateDate(),
+	ordering: '-added'
 };
 console.log('Parameter Check', params);
-
-// $('.platform-list').click(function() {
-// 	if ($(`input[type=checkbox][name=platform]:checked`).length) {
-// 		$('#platform-all').attr('checked', true);
-// 	} else {
-// 		$('#platform-all').attr('checked', false);
-// 	}
-// });
 
 //Take params set from above and format them into workable URL to fetch data.
 function formatParams(params) {
@@ -92,7 +39,7 @@ function formatParams(params) {
 //Per requirements of RAWG's API usage they want only a user-agent header.
 const opts = {
 	headers: {
-		'User-Agent': `<ClassProject> / <VER 0.09> <Currently in Alpha testing>`
+		'User-Agent': `<ClassProject> / <VER 0.02> <Currently in Alpha testing>`
 	}
 };
 
@@ -135,10 +82,10 @@ function mapResults(responseJson) {
 			//multiple items
 			platform: game.platforms,
 			genre: game.genres,
-			video: game.clip
+			video: game.clip,
+			date: game.released
 		};
 	});
-	console.log(gamedata);
 	inputData(gamedata);
 }
 
@@ -153,39 +100,39 @@ function inputData(gamedata) {
 		info += `<div class= "game-name">${input.name}</div>`;
 		info += `<div class="game-space"></div>`;
 		if (input.video === null) {
-			info += `<br><div class=no-clip>No preview was found.</div>`;
-			info += `<span><b>Platforms</b>: `;
+			info += `<div class=no-clip>No clips here yet!</div>`;
+			info += `<div class=released><span><b>Release Date</b>: ${input.date} `
+			info += `<div class="game-space"></div>`;
+			info += `<div class=platforms><span><b>Platforms</b>: `;
 			input.platform.forEach(b => {
 				info += `${b.platform.name} `;
 			});
-			info += `</span>`;
+			info += `</span></div>`;
 			info += `<div class="game-space"></div>`;
 			info += `<span><b>Genres</b>:`;
 			input.genre.forEach(c => {
-				info += `${c.name} `;
+				info += ` ${c.name} `;
 			});
 			info += `</span>`;
 			$(`#${input.slug}-card`).append(info);
-			const currentGame = $(`#${input.slug}-card`);
-			input.platform.forEach(b => {
-				$(currentGame).addClass(`pzz${b.platform.id}`);
-			});
-			input.genre.forEach(c => {
-				$(currentGame).addClass(`gzz${c.id}`);
-				console.log('!!!!!!!!!!!!!!!!', currentGame);
-			});
 			return undefined;
 		}
 		let result = Object.keys(input.video).map(function(key) {
 			return [Number(key), input.video[key]];
 		});
 		let videolink = result[1][1];
+		let videolink2 = result[2][1]
+		console.log(videolink2)
 		info += ` <div class= "game-clip">
 		<video width="280" height="158" controls>
   		<source src=" ${videolink.full}" type="video/mp4">
 		Your browser does not support the video tag.
 		</video>
 		</div>`;
+		info += `<div class="game-space"></div>`;
+		info += `<div class="videolink"><a href=https://www.youtube.com/embed/${videolink2}>See the full video</a></div>`
+		info += `<div class="game-space"></div>`;
+		info += `<div class=released><span><b>Release Date</b>: ${input.date} </span></div>`
 		info += `<div class="game-space"></div>`;
 		info += `<span><b>Platforms</b>: `;
 		input.platform.forEach(b => {
@@ -195,48 +142,15 @@ function inputData(gamedata) {
 		info += `<div class="game-space"></div>`;
 		info += `<span><b>Genres</b>: `;
 		input.genre.forEach(c => {
-			info += `${c.name} `;
+			info += ` ${c.name} `;
 		});
 		info += `</span>`;
 		info += `</div>`;
 		$(`#${input.slug}-card`).append(info);
-		let currentGame = $(`#${input.slug}-card`);
-		input.platform.forEach(b => {
-			$(currentGame).addClass(`${b.platform.id}pm`);
 		});
-		input.genre.forEach(c => {
-			$(currentGame).addClass(`${c.id}ge`);
-		});
-	});
 	loading = false;
 }
 
-function filterByPlatform() {
-	$('#platform-container').submit(e => {
-		e.preventDefault();
-		liveFilter();
-	});
-}
-
-//Based on checkboxs clicked this will hide games that do not fit the critera of the parameters.
-function liveFilter(gamedata) {
-	let platform = '';
-	gamedata.forEach(function(select) {
-		select.consoles.forEach(function(find) {
-			platform += find.platform.id;
-		});
-	});
-	$('input[type=checkbox][name=platform]').click(function() {
-		$('.game-card').hide();
-		$('input[type=checkbox][name=platform]:checked').each(
-			function() {
-				$('.platform')
-					.val()
-					.show();
-			}
-		);
-	});
-}
 
 //Check to ensure where the user is on the page. If they have reached  a point it will fetch more data from the next page.
 function infiniteScroll() {
@@ -270,7 +184,6 @@ function pageLoadClick() {
 		}
 		const searchParam = $('.search-param').val();
 		$('#card-list').empty();
-		$('#parameters').slideToggle();
 		pageNum = 1;
 		fetchGames(searchParam);
 	});
@@ -280,11 +193,6 @@ function pageLoadClick() {
 function initializeListeners() {
 	pageLoad();
 	infiniteScroll();
-	parameterButton();
-	genreButton();
-	contactButton();
-	platformButton();
-	disclosureButton();
 	pageLoadClick();
 }
 
